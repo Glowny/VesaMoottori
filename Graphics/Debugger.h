6c52cbac82a6f682440d0bb5f7121330f2e5355b
imTK <mokkak.tk@gmail.com>
2014-09-12 10:56:07 +0300

    Debugger
    Ensiversio

---
+++ Graphics/Debugger.h
@@ -0,0 +1,28 @@
+#ifndef DEBUGGER_H
+#define DEBUGGER_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <tchar.h>
+#include <GL\glew.h>
+#include <GL\GLU.h>
+#include <GL\GL.h>
+
+
+class Debugger
+{
+public:
+	Debugger();
+	~Debugger();
+	void CheckGlew();
+	void Type(std::string value);
+	void Variable(int vi); void Variable(float vf); void Variable(double vd); void Variable(std::string vs); void Variable(char vc);
+
+
+private:
+};
+
+
+#endif
+
