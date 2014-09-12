d1383b0e928cbd8e923c0ece8cef017b7cf50986
Grimcode <tti13stuukkak@kamk.fi>
2014-09-12 10:50:58 +0300

    Main Erroreita
    No niin.

--- Graphics/Main.cpp
+++ Graphics/Main.cpp
@@ -7,8 +7,57 @@
 #include <GL\GLU.h>
 #include <GL\GL.h>
 
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
 int main()
 {
+	WNDCLASSEX window;
+	window.cbSize = sizeof(WNDCLASSEX);
+	window.style = CS_OWNDC;
+	window.lpfnWndProc = WndProc;
+	window.cbClsExtra = 0;
+	window.cbWndExtra = 0;
+	window.hInstance = GetModuleHandle(nullptr);
+	window.hIcon = NULL;
+	window.hCursor = NULL;
+	window.hbrBackground = NULL;
+	window.lpszMenuName = NULL;
+	window.lpszClassName = (LPCTSTR)"Ikkuna";
+	window.hIconSm = NULL;
+
+	if (!RegisterClassEx(&window))
+	{
+		MessageBox(NULL,
+			_T("RegisterClassEx failed!"),
+			_T("asdasd"),
+			NULL);
+	}
+
+	HWND windowHandle = CreateWindow(
+		_T("IkkunaApp"),
+		_T("Titteli"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 100,
+		NULL,
+		NULL,
+		window.hInstance,
+		NULL);
+
+	if (!windowHandle)
+		std::cout << "WindowHandle failed!" << std::endl;
+
+	ShowWindow(windowHandle, SW_SHOWNORMAL);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	//return (int)msg.wParam;
+
 	//if(GLEW_OK!=glewInit())
 	GLenum error = glewInit();
 	if (error == GLEW_OK)
--- Graphics/WindowHandler.h
+++ Graphics/WindowHandler.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <windows.h>
+#define WIN32_LEAN_AND_MEAN
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
 class WindowHandler
 {
 public:
-	WindowHandler();
-	~WindowHandler();
-};
+	//static WindowHandler* getInstance();
+	static bool Initialize();
 
+private:
+	WindowHandler() {};
+	~WindowHandler() {};
+	//LRESULT *procPointer;
+	//WNDCLASSEX *winPoint; // Pointer to the window.
+};
