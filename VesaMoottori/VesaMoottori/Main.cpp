6a607a3c54661e4aceceffa5fb8cd683f25f18ed
Grimcode <tti13stuukkak@kamk.fi>
2014-09-23 10:56:48 +0300

    OpenGL Alustus Mainissa
    Kissat on koiria.

--- VesaMoottori/VesaMoottori/Main.cpp
+++ VesaMoottori/VesaMoottori/Main.cpp
@@ -7,27 +7,30 @@
 #include <GL\glew.h>
 #include <GL\GLU.h>
 #include <GL\GL.h>
-#include "Debugger.h"
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-bool GlewTests(); // Palauttaa true jos glew testit onnistuu.
-bool InitGL(GLvoid); // Alustetaan OpenGL.
-bool DrawGLScene(GLvoid); // Alustetaan ikkuna piirtämistä varten.
+//bool GlewTests(); // Palauttaa true jos glew testit onnistuu.
+//bool InitGL(GLvoid); // Alustetaan OpenGL.
+//bool DrawGLScene(GLvoid); // Alustetaan ikkuna piirtämistä varten.
 //bool CreateGLWindow();
 //GLvoid CloseGLWindow(GLvoid); // Vapauttaa asioita kun ohjelma suljetaan.
 
 int main()
 {
-	const TCHAR		winName[] = TEXT("Window");
-	const TCHAR		winClassName[] = TEXT("Window");
-	MSG				messages;
-	bool			isRunning = true;
-	HGLRC			hRC = NULL; // Permanent rendering context.
-	HDC				hDC = NULL; // Private GDI device context.
-	HWND			winHandle = NULL; // Holds window handle.
-	HINSTANCE		hInstance = GetModuleHandle(nullptr); // Instance of the application
-	WNDCLASSEX		winClass; // Sisältää ikkunan asetukset.
+	const TCHAR				winName[] = TEXT("Window");
+	const TCHAR				winClassName[] = TEXT("Window");
+	MSG						messages;
+	HGLRC					hRC = NULL; // Permanent rendering context.
+	HDC						hDC = NULL; // Private GDI device context.
+	HGLRC					hGLRC = NULL; // OpenGL rendering context.
+	HWND					winHandle = NULL; // Holds window handle.
+	HINSTANCE				hInstance = GetModuleHandle(nullptr); // Instance of the application
+	WNDCLASSEX				winClass; // Sisältää ikkunan asetukset.
+	PIXELFORMATDESCRIPTOR	winPixel; // Describes the pixel format of a drawing surface.
+	bool					isRunning = true;
+	int						pixFormat = NULL;
 
+	// Alustetaan windows-ikkkuna.
 	winClass.lpfnWndProc	= WndProc;
 	winClass.cbSize			= sizeof(WNDCLASSEX);
 	winClass.style			= CS_OWNDC;
@@ -41,7 +44,7 @@ int main()
 	winClass.lpszMenuName	= NULL;
 	winClass.hIconSm		= NULL;
 
-	if (!RegisterClassEx(&winClass)) // Rekisteröidään ikkuna.
+	if (!RegisterClassEx(&winClass)) // Rekisteröidään ikkuna ja tarkistetaan onnistuuko se.
 		std::cout << "RegisterClassEx failed!" << std::endl;
 	else
 		std::cout << "RegisterClassEx succeeded!" << std::endl;
@@ -57,16 +60,41 @@ int main()
 		hInstance,
 		NULL);
 
-	if (!winHandle)
+	if (!winHandle) // Tarkastetaan handlen luominen.
 		std::cout << "WindowHandle failed!" << std::endl;
 	else
 		std::cout << "WindowHandle succeeded!" << std::endl;
 
+	hDC = GetDC(winHandle); // Asetetaan arvo aiemmin alustetulle device contextille.
+
+	// Alustetaan OpenGL varten.
+	winPixel.nSize			= sizeof(PIXELFORMATDESCRIPTOR);
+	winPixel.nVersion		= 1;
+	winPixel.dwFlags		= PFD_DOUBLEBUFFER | PFD_DRAW_TO_WINDOW | LPD_SUPPORT_OPENGL;
+	winPixel.iPixelType		= PFD_TYPE_RGBA;
+	winPixel.cColorBits		= 24;
+	winPixel.cDepthBits		= 24;
+
+	pixFormat = ChoosePixelFormat(hDC, &winPixel); // Etsitään lähin pixeliformaatti jota voidaan käyttää.
+	if (!SetPixelFormat(hDC, pixFormat, &winPixel))
+		std::cout << "SetPixelFormat failed!" << std::endl;
+	else
+		std::cout << "SetPixelFormat succeeded!" << std::endl;
+
+	hGLRC = wglCreateContext(hDC); // Luodaan handle OpenGL renderöintiä varten.
+	wglMakeCurrent(hDC, hGLRC); // Käytetään tätä ikkunaa komentokutsuissa.
+
 	ShowWindow(winHandle, SW_SHOWNORMAL); // Näytetään rekisteröity ikkuna.
 	UpdateWindow(winHandle);
+	//GlewTests(); // Testaa OpenGL 2.1 toimivuutta.
+
+
+	// Jotain testausta.
+	glClearColor(1.0f, 0.2f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
 	
-	GlewTests(); // Testaa OpenGL 2.1 toimivuutta.
-	
+
+
 	while (isRunning) // Ohjelman main-looppi.
 	{
 		while (PeekMessage(&messages, NULL, 0, 0, PM_REMOVE)) // Main loop jossa ikkuna ottaa vastaan viestejä.
@@ -75,9 +103,11 @@ int main()
 			if (messages.message == WM_QUIT)
 			{
 				isRunning = false;
+				wglDeleteContext(hGLRC); // Tuhotaan renderöinti sisältö.
 				break;
 			}
 			DispatchMessage(&messages);
+			SwapBuffers(hDC); // Swapataan buffereita piirtoa varten.
 		}
 	}
 
@@ -108,44 +138,44 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 	return 0;
 } 
 
-bool GlewTests()
-{
-	GLenum error = glewInit(); // Alustetaan Glew.
-	if (error == GLEW_OK)
-		std::cout << "GLEW succeeded!" << std::endl;
-	else
-	{
-		std::cout << "GLEW failed, error message: " << glewGetErrorString(error) << std::endl;
-		return false;
-	}
-
-	if (GLEW_VERSION_2_1) // Tarkastetaan onko 2.1 käytössä.
-		std::cout << "OpenGL 2.1 supported." << std::endl;
-	else
-	{
-		std::cout << "OpenGL 2.1 not supported." << std::endl;
-		return false;
-	}
-	return true;
-}
-
-bool InitGL(GLvoid)
-{
-	glShadeModel(GL_SMOOTH); // Enables smooth shading.
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Musta tausta.
-	glClearDepth(1.0f); // Syvyys bufferin setup.
-	//glEnable(GL_DEPTH_TEST); // Syvyys testejä.
-	//glDepthFunc(GL_LEQUAL);
-	return true;
-}
-
-bool DrawGLScene(GLvoid)
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clearaa ruudun ja syvyys bufferin.
-	glLoadIdentity(); // Resettaa modelview matrixin.
-	return true;
-}
-
+//bool GlewTests()
+//{
+//	GLenum error = glewInit(); // Alustetaan Glew.
+//	if (error == GLEW_OK)
+//		std::cout << "GLEW succeeded!" << std::endl;
+//	else
+//	{
+//		std::cout << "GLEW failed, error message: " << glewGetErrorString(error) << std::endl;
+//		return false;
+//	}
+//
+//	if (GLEW_VERSION_2_1) // Tarkastetaan onko 2.1 käytössä.
+//		std::cout << "OpenGL 2.1 supported." << std::endl;
+//	else
+//	{
+//		std::cout << "OpenGL 2.1 not supported." << std::endl;
+//		return false;
+//	}
+//	return true;
+//}
+//
+//bool InitGL(GLvoid)
+//{
+//	glShadeModel(GL_SMOOTH); // Enables smooth shading.
+//	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Musta tausta.
+//	glClearDepth(1.0f); // Syvyys bufferin setup.
+//	//glEnable(GL_DEPTH_TEST); // Syvyys testejä.
+//	//glDepthFunc(GL_LEQUAL);
+//	return true;
+//}
+//
+//bool DrawGLScene(GLvoid)
+//{
+//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clearaa ruudun ja syvyys bufferin.
+//	glLoadIdentity(); // Resettaa modelview matrixin.
+//	return true;
+//}
+//
 //bool CreateGLWindow()
 //{
 //	GLuint		PixelFormat;
