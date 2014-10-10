2831178a2ead80f5dd0656b3366e1be4ccb54ed9
Glowny <Nuudelitlautasella@hotmail.com>
2014-10-10 11:39:29 +0300

    Resussinhallintaa käyttää maini
    Hyi ku ruma

--- VesaMoottori/VesaMoottori/ResourceManager.h
+++ VesaMoottori/VesaMoottori/ResourceManager.h
@@ -5,25 +5,26 @@
 #include <string>
 #include <fstream>
 #include "lodepng.h"
+#include "ImageInfo.h"
 
 typedef std::vector<unsigned char> DecodedImage;	// voitais käyttää projektissa enemmän
 
-class ResourceManager
-{
-public:
-	ResourceManager() {};
-	~ResourceManager() {};
 
-	void LoadImage(std::string filename);			// upitaan kuva mappiin
-	DecodedImage* FindImage(std::string filename);	//heateaan kuva mapista
-	void AddImageLocation(std::string filename);	// ei käytetä mihinkään atm. IGNORE
+	class ResourceManager
+	{
+	public:
+		ResourceManager() {};
+		~ResourceManager() {};
 
-	char* LoadShader(std::string filename);	// palauttaa tällä hetkellä shaderin & uppii shadermappiin
-	char* FindShader(std::string filename);		// palauttaa shaderin shadermapista
-private:
-	std::map<unsigned int, char*> shaders;		//shadermap
-	std::map<unsigned int, DecodedImage> decodedImages; // imagelocationista kuva, joka dekoodataan ja pistetään tänne. Tämä ottaa myös nimen
-	std::vector<std::string> imageLocations; // ei ehkä välttämätön. Helppo muokattavuus jos vaikka tekstifileessä, josta voidaan sitten hakea kaikki tarvittavat kuvat.
-	unsigned int MyHasher(std::string filename);	//muuttaa stringin uchariksi;
-};
+		void RLoadImage(std::string filename);			// upitaan kuva mappiin
+		ImageInfo* FindImage(std::string filename);	//heateaan kuva mapista
+		void AddImageLocation(std::string filename);	//kuvan sijainti vektoriin, jos vaikka käynnistäessä la
+		char* LoadShader(std::string filename);	// palauttaa tällä hetkellä shaderin & uppii shadermappiin
+		char* FindShader(std::string filename);		// palauttaa shaderin shadermapista
+	private:
+		std::map<unsigned int, char*> shaders;		//shadermap
+		std::map<unsigned int, ImageInfo> decodedImages; // imagelocationista kuva, joka dekoodataan ja pistetään tänne. Tämä ottaa myös nimen
+		std::vector<std::string> imageLocations; // ei ehkä välttämätön. Helppo muokattavuus jos vaikka tekstifileessä, josta voidaan sitten hakea kaikki tarvittavat kuvat.
+		unsigned int MyHasher(std::string filename);	//muuttaa stringin uchariksi;
+	};
 
